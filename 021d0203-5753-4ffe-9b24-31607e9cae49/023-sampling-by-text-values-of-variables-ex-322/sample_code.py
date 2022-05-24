75a3723777388cea9d6e32eed764c30547f46b65
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:00 +0300

    Created sample_code for 023-sampling-by-text-values-of-variables-ex-322

---
+++ 021d0203-5753-4ffe-9b24-31607e9cae49/023-sampling-by-text-values-of-variables-ex-322/sample_code.py
@@ -0,0 +1,8 @@
+# Города SFO (Сибирский федеральный округ)
+sfo = ["Novosibirsk", "Angarsk", "Krasnoyarsk", "Norilsk"]
+
+# Фильтр по городам SFO
+sfo_census = ____
+
+# Распечатайте результат
+print(sfo_census)
