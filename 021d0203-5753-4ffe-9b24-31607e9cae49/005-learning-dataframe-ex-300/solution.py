8e3bdcea286946d8ac6a980fdf53f85c512d1df8
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:42:24 +0300

    Created solution for 005-learning-dataframe-ex-300

---
+++ 021d0203-5753-4ffe-9b24-31607e9cae49/005-learning-dataframe-ex-300/solution.py
@@ -0,0 +1,8 @@
+# Распечатайте первые несколько строк: census
+print(census.head())
+
+# Распечатайте информацию: census
+print(census.info())
+
+# Распечатайте количество строк и столбцов: census
+print(census.shape)
