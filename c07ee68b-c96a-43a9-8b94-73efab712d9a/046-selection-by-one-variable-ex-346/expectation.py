e347c21edf8a5ca70f4f97c5a46a637dc290c69a
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:48 +0300

    Created expectation for 046-selection-by-one-variable-ex-346

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/046-selection-by-one-variable-ex-346/expectation.py
@@ -0,0 +1,10 @@
+
+
+Ex().multi(
+    check_function('sales.pivot_table', 0, signature=False).multi(
+        check_args(0).has_equal_value(),
+        check_args(1).has_equal_value(),
+    ).has_equal_value(),
+    check_object('mean_sales_by_type').has_equal_value(),
+    check_function('print', 0).check_args(0).has_equal_value(),
+)
