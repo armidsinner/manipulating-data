134d93ceeb14999b4f46194fe2087565790a03dc
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:33 +0300

    Created solution for 069-analysis-of-variables-data

---
+++ 90314171-60e8-4dcf-8790-4a3742f3305d/069-analysis-of-variables-data/solution.py
@@ -0,0 +1,5 @@
+# Точечный график nb_sold против avg_price с заголовком
+apples.plot(x="nb_sold", y="avg_price", kind="scatter", title="Number of apples sold vs. average price")
+
+# Покажите график
+plt.show()
