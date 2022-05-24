425ec5febea649e3372a2a518777679db20a45a5
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:53 +0300

    Created sample_code for 048-selection-by-one-variable-ex-346

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/048-selection-by-one-variable-ex-346/sample_code.py
@@ -0,0 +1,5 @@
+# Получите среднее значение weekly_sales по столбцам type и is_holiday 
+mean_sales_by_type_holiday = sales.pivot_table(____)
+
+# Распечатайте mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
