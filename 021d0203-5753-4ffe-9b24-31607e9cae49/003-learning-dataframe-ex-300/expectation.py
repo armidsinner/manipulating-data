0d7a1d96e22cbfd1a4c547f340b7e6a3e8a7cc1d
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:42:20 +0300

    Created expectation for 003-learning-dataframe-ex-300

---
+++ 021d0203-5753-4ffe-9b24-31607e9cae49/003-learning-dataframe-ex-300/expectation.py
@@ -0,0 +1,6 @@
+Ex().multi(
+    check_function("print", 0).check_correct(
+        check_args(0).has_equal_value(),
+        check_function("census.head", signature=""),
+    ).has_equal_value()
+)
