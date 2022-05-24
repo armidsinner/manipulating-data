e6f35b69a94ae8e79d45262775541a4320dc31cd
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:26 +0300

    Created sample_code for 065-calculations-in-a-pivot-table

---
+++ 3ef9cd3c-c012-420b-b537-6bad674da167/065-calculations-in-a-pivot-table/sample_code.py
@@ -0,0 +1,11 @@
+# Получить среднюю температуру по всему миру по годам
+mean_temp_by_year = temp_by_country_city_vs_year.____
+
+# Фильтр для года, в котором была самая высокая средняя температура
+print(mean_temp_by_year[____])
+
+# Получить среднюю температуру по городам
+mean_temp_by_city = temp_by_country_city_vs_year.____
+
+# Фильтр для города с самой низкой средней температурой
+print(mean_temp_by_city[____])
