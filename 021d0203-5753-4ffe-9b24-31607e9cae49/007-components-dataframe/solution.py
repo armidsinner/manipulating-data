abf7fb970487f04cb83f0c445f7951e983bb5d83
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:42:29 +0300

    Created solution for 007-components-dataframe

---
+++ 021d0203-5753-4ffe-9b24-31607e9cae49/007-components-dataframe/solution.py
@@ -0,0 +1,11 @@
+# Импортируйте pandas, используя псевдоним pd
+import pandas as pd
+
+# Распечатайте значения census
+print(census.values)
+
+# Распечатайте названия столбцов census
+print(census.columns)
+
+# Распечатайте индекс строк census
+print(census.index)
