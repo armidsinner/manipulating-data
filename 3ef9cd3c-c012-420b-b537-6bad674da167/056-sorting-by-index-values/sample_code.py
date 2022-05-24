b94f5c86e5122fbe3e64ee8a2b4a852fdf6fdc8e
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:09 +0300

    Created sample_code for 056-sorting-by-index-values

---
+++ 3ef9cd3c-c012-420b-b537-6bad674da167/056-sorting-by-index-values/sample_code.py
@@ -0,0 +1,8 @@
+# Отсортируйте temperatures_ind по значениям индекса
+print(____)
+
+# Отсортируйте temperatures_ind по значениям индексов на уровне city
+print(____)
+
+# Отсортируйте temperatures_ind по country затем по убыванию city
+print(____)
