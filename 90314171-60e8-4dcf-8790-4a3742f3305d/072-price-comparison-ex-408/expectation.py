5af2b4670848543575b6098b1e7f7f2393ac7d00
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:38 +0300

    Created expectation for 072-price-comparison-ex-408

---
+++ 90314171-60e8-4dcf-8790-4a3742f3305d/072-price-comparison-ex-408/expectation.py
@@ -0,0 +1,16 @@
+
+
+Ex().multi(
+    check_function("apples.hist", index=0, signature=False).check_args(0).has_equal_output(),
+    check_function("apples.hist", index=0, signature=False).has_equal_ast(
+        incorrect_msg="Правильно ли вы подбираете `apples` для типа `conventional`?"),
+
+    check_function("apples.hist", index=1, signature=False).check_args(0).has_equal_output(),
+    check_function("apples.hist", index=1, signature=False).has_equal_ast(
+        incorrect_msg="Правильно ли вы подбираете `apples` для типа `organic`?"),
+
+    check_function('matplotlib.pyplot.legend', 0, signature=False).multi(
+        check_args(0).has_equal_value(),
+    ),
+    check_function('matplotlib.pyplot.show', 0, signature=False).has_equal_value(),
+)
