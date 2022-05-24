360ceacfbca13fcc4acb1eea45ccf4fe517cb48d
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:51 +0300

    Created sample_code for 047-selection-by-one-variable-ex-346

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/047-selection-by-one-variable-ex-346/sample_code.py
@@ -0,0 +1,8 @@
+# Импортируйте numpy с названием np
+import numpy as np
+
+# Получите среднее значение и медиану weekly_sales для каждого типа магазинов
+mean_med_sales_by_type = sales.pivot_table(____)
+
+# Распечатайте mean_med_sales_by_type
+print(mean_med_sales_by_type)
