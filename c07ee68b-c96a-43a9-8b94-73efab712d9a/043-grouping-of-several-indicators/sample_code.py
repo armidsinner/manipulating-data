d5cdba7e9fb2669125f9d14b98c15a11f4a93cfc
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:41 +0300

    Created sample_code for 043-grouping-of-several-indicators

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/043-grouping-of-several-indicators/sample_code.py
@@ -0,0 +1,14 @@
+# Импортируйте numpy с названием np
+____
+
+# Получите weekly_sales для каждого типа магазинов: получите min, max, mean, и median
+sales_stats = ____
+
+# Распечатайте sales_stats
+print(sales_stats)
+
+# Получите unemployment и fuel_price_usd_per_l для каждого типа магазинов: получите min, max, mean, и median
+unemp_fuel_stats = ____
+
+# Распечатайте unemp_fuel_stats
+print(unemp_fuel_stats)
