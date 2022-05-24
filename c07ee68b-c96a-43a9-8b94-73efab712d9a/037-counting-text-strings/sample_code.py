ed69e10a8a64f01f472a43bceec3962b9cf88dba
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:31 +0300

    Created sample_code for 037-counting-text-strings

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/037-counting-text-strings/sample_code.py
@@ -0,0 +1,15 @@
+# Подсчитайте количество stores каждого type
+store_counts = ____
+print(store_counts)
+
+# Посчитайте долю stores каждого type
+store_props = ____
+print(store_props)
+
+# Подсчитайте количество номеров department и отсортируйте
+dept_counts_sorted = ____
+print(dept_counts_sorted)
+
+# Подсчитайте количество номеров departments и отсортируйте
+dept_props_sorted = ____.____(sort=____, normalize=____)
+print(dept_props_sorted)
