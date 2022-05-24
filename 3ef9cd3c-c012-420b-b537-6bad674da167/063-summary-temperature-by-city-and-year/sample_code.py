a8366b4d7d7f26fbd57be16ec0061254ff87e2e6
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:22 +0300

    Created sample_code for 063-summary-temperature-by-city-and-year

---
+++ 3ef9cd3c-c012-420b-b537-6bad674da167/063-summary-temperature-by-city-and-year/sample_code.py
@@ -0,0 +1,8 @@
+# Добавьте столбец year в temperatures
+____
+
+# Сводная таблица avg_temp_c по country и city в year
+temp_by_country_city_vs_year = ____
+
+# Распечатайте результат
+print(temp_by_country_city_vs_year)
