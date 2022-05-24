53ce089d14e7f442d97fb92efff300a9cd5f16ee
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:25 +0300

    Created sample_code for 034-cumulative-results

---
+++ c07ee68b-c96a-43a9-8b94-73efab712d9a/034-cumulative-results/sample_code.py
@@ -0,0 +1,11 @@
+# Отсортируйте строки sales_1_1 по столбцу date
+sales_1_1 = ____
+
+# Получите сумму еженедельных продаж, добавив в cum_weekly_sales
+sales_1_1[____] = ____
+
+# Получите максимальную сумму weekly_sales и добавьте ее в столбец cum_max_sales
+____
+
+# Распечатайте столбцы, которые вы рассчитали
+print(sales_1_1[["date", "weekly_sales", "cum_weekly_sales", "cum_max_sales"]])
