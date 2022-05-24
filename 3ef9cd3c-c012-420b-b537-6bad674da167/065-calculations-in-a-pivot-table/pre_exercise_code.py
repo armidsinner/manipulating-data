5e7e7b26ce74d249bdb8c2629aa10ecc5af783bd
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:26 +0300

    Created pre_exercises_code for 065-calculations-in-a-pivot-table

---
+++ 3ef9cd3c-c012-420b-b537-6bad674da167/065-calculations-in-a-pivot-table/pre_exercise_code.py
@@ -0,0 +1,5 @@
+import pandas as pd
+temperatures = pd.read_csv("temperatures.csv")
+temperatures['date'] = pd.to_datetime(temperatures['date'], errors='coerce')
+temperatures["year"] = temperatures["date"].dt.year
+temp_by_country_city_vs_year = temperatures.pivot_table("avg_temp_c", index=["country", "city"], columns="year")
