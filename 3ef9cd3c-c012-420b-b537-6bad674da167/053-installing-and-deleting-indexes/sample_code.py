6c704ccb561e8223cab08c8100b3ab7e2bac149a
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:44:03 +0300

    Created sample_code for 053-installing-and-deleting-indexes

---
+++ 3ef9cd3c-c012-420b-b537-6bad674da167/053-installing-and-deleting-indexes/sample_code.py
@@ -0,0 +1,14 @@
+# Распечатайте temperatures
+print(____)
+
+# Установите индекс temperatures для city
+temperatures_ind = ____
+
+# Распечатайте temperatures_ind
+print(____)
+
+# Сбросьте индекс, сохранив его содержимое
+print(____)
+
+# Сбросьте индекс, удалив его содержимое
+print(____)
