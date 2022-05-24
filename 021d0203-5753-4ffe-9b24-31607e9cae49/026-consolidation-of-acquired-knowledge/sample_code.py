0ab1ec0eaa4ebef5c533c2fe0eede1352efc25b5
armidsinner <69764302+armidsinner@users.noreply.github.com>
2022-05-24 19:43:07 +0300

    Created sample_code for 026-consolidation-of-acquired-knowledge

---
+++ 021d0203-5753-4ffe-9b24-31607e9cae49/026-consolidation-of-acquired-knowledge/sample_code.py
@@ -0,0 +1,14 @@
+# Создайте indiv_per_10k, содержащий число одиноких на десять тысяч человек в каждом регионе
+census["indiv_per_10k"] = 10000 * ____ / ____ 
+
+# Фильтр для строк indiv_per_10k больше 20 
+high_census = ____
+
+# Отсортируйте high_census по убыванию indiv_per_10k
+high_census_srt = ____
+
+# Из high_census_srt выберите столбцы city и indiv_per_10k
+result = ____
+
+# Распечатайте результат
+print(result)
